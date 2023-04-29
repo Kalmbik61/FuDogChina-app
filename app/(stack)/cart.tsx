81c5e20d7f21c42306a9dc88a5c4fc6c872caee3
Evgeniy <kalmbik61@gmail.com>
2023-04-29 19:44:18 +0300

    f

--- app/(stack)/cart.tsx
+++ app/(stack)/cart.tsx
@@ -1,22 +1,16 @@
 import { SafeAreaView, Platform, KeyboardAvoidingView } from "react-native";
 import Cart from "../../components/Cart/Cart";
 import { COLORS } from "../../constants/Colors";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function CartScreen() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.darkBg }}>
-          <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-          >
-            <Cart />
-          </KeyboardAvoidingView>
-        </SafeAreaView>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.darkBg }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <Cart />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
